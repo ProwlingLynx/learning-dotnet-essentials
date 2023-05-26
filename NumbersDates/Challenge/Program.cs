776fb02bc81691a107052cca53350d4ef6b62762
Javier Tamez <tamezjavier@gmail.com>
2023-05-26 16:56:24 -0500

    Added first attempt at date time challenge.

---
+++ NumbersDates/Challenge/Program.cs
@@ -0,0 +1,39 @@
+﻿
+while (true)
+{
+  string userInput = "";
+
+  Console.WriteLine("Which date? (or 'exit')");
+
+  userInput = Console.ReadLine();
+  if (userInput == "exit")
+  {
+    break;
+  }
+
+  DateTime userDate;
+
+  bool isDate = DateTime.TryParse(userInput, out userDate);
+
+  if (isDate)
+  {
+    TimeSpan timeDifference = userDate - DateTime.Now;
+    int daysDifference = timeDifference.Days;
+    if (daysDifference < 0)
+    {
+      Console.WriteLine($"That date went by {daysDifference * -1} days ago.");
+    }
+    else if (daysDifference > 0)
+    {
+      Console.WriteLine($"That date is in {daysDifference} days.");
+    }
+    else if (daysDifference == 0)
+    {
+      Console.WriteLine($"That is today's date.");
+    }
+  }
+  else
+  {
+    Console.WriteLine($"'{userInput}' is not a valid date");
+  }
+}
